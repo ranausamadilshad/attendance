cba86b76534a7603950d6d4a3d79b6d912567e0d
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-08-31 12:55:55 +0500

    View Employees Screen Added

--- src/Component/ViewEmployees/ViewEmployeesScreen.js
+++ src/Component/ViewEmployees/ViewEmployeesScreen.js
@@ -1,35 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ViewEmployees.css'
 import '../CreateDepartment/CreateDepartment.css';
 
 
 
 const ViewEmployeesScreen = () => {
+    const [toggle, settoggle] = useState(false);
     return (
         <>
           
- <section class="view_department_section">
+ <section class="view_created_employee_section">
 
-<div class="view_department_table"> 
+<div class="view_created_employee_table"> 
     <table>
       
-        <tr class="table_header">
+        <tr class="created_employee_table_header">
+        <th>Image</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Join Date</th>
+            <th>Department</th>
             <th>Job Title</th>
-            <th>Allowed Leaves</th>
-            <th class="view_department_action">Actions</th>
+            <th>Job shift</th>
+            <th>Date Of Birth</th>
+            <th>Gender</th>
+            <th>Address</th>
+            <th class="view_created_employee_action">Actions</th>
         </tr>
        
-        <tr class="table_body">
+        <tr class="created_employee_table_body">
            
             <td>Health Care Center</td>
             <td>20</td>
+            <td>20</td>
+            <td>20</td>
+             <td>20</td>
+             <td>20</td>
+             <td>20</td>
+             <td>20</td>
+             <td>20</td>
+             <td>20</td>
+             <td>20</td>
             <td>
-                   <div class="view_department_action_btn view_department_action">
-                      <i class="fas fa-ellipsis-v"></i>
-                      <div class="view_department_do_action">
+                   <div class="view_created_employee_action_btn view_created_employee_action">
+                      <i class="fas fa-ellipsis-v" onClick={()=>{toggle?settoggle(false):settoggle(true)}}></i>
+                      <div style={{display:toggle?'block':'none'}}>
+                      <div class="view_created_employee_do_action">
                         <a href="#"><i class="fas fa-pen"></i><span>Edit</span></a>
                         <a href="#"><i class="fas fa-trash-alt"></i><span>Delete</span></a>
                        </div> 
+                       </div>
                     </div>
                
                    
