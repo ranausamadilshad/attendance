6cec54e8848309785a09d24a3ee40c71a6d1e210
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-09-01 14:06:51 +0500

    Update Department Form Validated

--- src/Component/ViewDepartment/UpdateDepartment.js
+++ src/Component/ViewDepartment/UpdateDepartment.js
@@ -1,55 +1,121 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "../FormControl/FormikControl";
 
 const UpdateDepartment = () => {
-    return (
-        <>
-<section className="Viwe_Department_edit_page">
-  <div className="modal fade"  id="UpdateDepartment" tabindex="-1" role="dialog" aria-labelledby="UpdateDepartmentTitle" aria-hidden="true">
-    <div className="modal-dialog" role="document">
-      <div className="modal-content" style={{padding:"0px 10px"}}>
-        <div className="modal-header" style={{padding:"1rm 5px !important"}}>
-          <h5 className="modal-title" id="UpdateDepartmentTitle">Update Department</h5>
-          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <section className="create_department_form">
-            <div className="create_department_container">
-                  <form>
-                     <div className="create_department_form_fields"> 
-                         
-                      <div className="input_field">
-                      <label>Department Name <span className="mandatory"> *</span></label>
-                      <input type="text" placeholder=""/>
-                      </div>
-                      <div className="input_field">
-                      <label>Phone <span className="mandatory"> * </span></label>
-                      <input type="number" placeholder="" required/>
-                      </div>
-                      <div className="input_field">
-                        <label>Department Email <span className="Optional">(Optional)</span></label>
-                        <input type="email" placeholder=""/>
+  const initialValues = {
+    email: "",
+    dName: "",
+    dAddress: "",
+    Phone: "",
+  };
+
+  const validationSchema = Yup.object({
+    dName: Yup.string().required("Required"),
+    Phone: Yup.number()
+      .positive("A phone number can't start with a minus")
+      .integer("A phone number can't include a decimal point")
+      .max(11)
+      .required("Required"),
+  });
+
+  const onSubmit = (values) => {
+    console.log("Create Department data", values);
+  };
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => (
+          <section className="Viwe_Department_edit_page">
+            <div
+              className="modal fade"
+              id="UpdateDepartment"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="UpdateDepartmentTitle"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog" role="document">
+                <div className="modal-content" style={{ padding: "0px 10px" }}>
+                  <div
+                    className="modal-header"
+                    style={{ padding: "1rm 5px !important" }}
+                  >
+                    <h5 className="modal-title" id="UpdateDepartmentTitle">
+                      Update Department
+                    </h5>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <section className="create_department_form">
+                    <div className="create_department_container">
+                      <Form>
+                        <div className="create_department_form_fields">
+                          <div className="input_field">
+                            <label>
+                              Department Name{" "}
+                              <span className="mandatory"> *</span>
+                            </label>
+                            <FormikControl control="input" type="text" name="dName" />
+                            {/* <input type="text" placeholder="" /> */}
+                          </div>
+                          <div className="input_field">
+                            <label>
+                              Phone <span className="mandatory"> * </span>
+                            </label>
+                            <FormikControl
+                        control="input"
+                        type="number"
+                        name="Phone"
+                      />
+                            {/* <input type="number" placeholder="" required /> */}
+                          </div>
+                          <div className="input_field">
+                            <label>
+                              Department Email{" "}
+                              <span className="Optional">(Optional)</span>
+                            </label>
+                            <FormikControl
+                        control="input"
+                        type="email"
+                        name="email"
+                      />
+                            {/* <input type="email" placeholder="" /> */}
+                          </div>
+                          <div className="input_field">
+                            <label>
+                              Departmant Address{" "}
+                              <span className="Optional">(Optional)</span>
+                            </label>
+                            <FormikControl control="textarea" name="dAddress" />
+                            {/* <textarea></textarea> */}
+                          </div>
+                          <div className="submit_btn">
+                            <button type="submit">Update</button>
+                          </div>
                         </div>
-                        <div className="input_field">
-                        <label>Departmant Address <span className="Optional">(Optional)</span></label>
-                        <textarea></textarea>
-                        </div>
-                      <div className="submit_btn">
-                      <button type="submit">Update</button>
-                      </div>
-                  
-                     </div> 
-                  </form>
+                      </Form>
+                    </div>
+                  </section>
+                </div>
+              </div>
             </div>
-        </section>
+          </section>
+        )}
+      </Formik>
+    </>
+  );
+};
 
-        
-      </div>
-    </div>
-  </div>
-</section>
-        </>
-    )
-}
-
-export default UpdateDepartment
+export default UpdateDepartment;
